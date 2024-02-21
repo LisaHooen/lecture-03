4138743b55d6d94f77d7bf9898fc7f70f59c0e93
lisahooen <liho046@student.kristiania.no>
2024-02-21 10:35:13 +0100

    toggling kommunelayer

--- src/Components/application/MapApplication.tsx
+++ src/Components/application/MapApplication.tsx
@@ -1,14 +1,14 @@
 import { Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
-import React, { MutableRefObject, useEffect, useRef } from "react";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
 import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
 import KommuneLayerCheckbox from "../kommuner/KommuneLayerCheckbox";
+import Layer from "ol/layer/Layer";
 
 useGeographic();
 
 const map = new Map({
-  layers: [new TileLayer({ source: new OSM() })],
   view: new View({ center: [10, 59], zoom: 8 }),
 });
 
@@ -23,10 +23,16 @@ const MapApplication = () => {
       });
     });
   };
+
+  const [layers, setLayers] = useState<Layer[]>([
+    new TileLayer({ source: new OSM() }),
+  ]);
+
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
   useEffect(() => {
     map.setTarget(mapRef.current);
   }, []);
+  useEffect(() => map.setLayers(layers), [layers]);
   return (
     <>
       <header>
@@ -36,7 +42,7 @@ const MapApplication = () => {
         <a href={"#"} onClick={handleFocusUser}>
           Focus on me
         </a>
-        <KommuneLayerCheckbox />
+        <KommuneLayerCheckbox setLayers={setLayers} />
       </nav>
       <div ref={mapRef}>
         <h1></h1>
--- src/Components/kommuner/KommuneLayerCheckbox.tsx
+++ src/Components/kommuner/KommuneLayerCheckbox.tsx
@@ -1,7 +1,28 @@
-import React, { useState } from "react";
+import Layer from "ol/layer/Layer";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { GeoJSON } from "ol/format";
 
-const KommuneLayerCheckbox = () => {
+const kommuneLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/lecture-03/kommuner.json",
+    format: new GeoJSON(),
+  }),
+});
+
+const KommuneLayerCheckbox = ({
+  setLayers,
+}: {
+  setLayers: Dispatch<SetStateAction<Layer[]>>;
+}) => {
   const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    if (checked) {
+      setLayers((old) => [...old, kommuneLayer]);
+    }
+    return () => setLayers((old) => old.filter((l) => l !== kommuneLayer));
+  }, [checked]);
   return (
     <>
       <div>
